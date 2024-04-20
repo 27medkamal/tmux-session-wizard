023e2052878abe716520bb544a960112157e8afa
Marek Maksimczyk <marek.maksimczyk@foundever.com>
2024-04-20 10:17:15 +0200

    Add way to create dev-env for aaarch64 and x86_64 (both linux and darwin)

--- flake.nix
+++ flake.nix
@@ -1,44 +1,47 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, ... }:
-
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      devPackages = [
-        (pkgs.bats.withLibraries (p: [ p.bats-support p.bats-assert ]))
-        pkgs.watchexec
-      ];
-      plugin = pkgs.tmuxPlugins.mkTmuxPlugin {
-        pluginName = "session-wizard";
-        rtpFilePath = "session-wizard.tmux";
-        version = "unstable";
-        src = self;
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        postInstall = ''
-          substituteInPlace $target/session-wizard.tmux \
-            --replace  \$CURRENT_DIR/session-wizard.sh $target/session-wizard.sh
-          wrapProgram $target/session-wizard.sh \
-            --prefix PATH : ${with pkgs; lib.makeBinPath ([ fzf zoxide coreutils gnugrep gnused ])}
-        '';
-      };
-    in
-    {
-      packages.x86_64-linux.dev = pkgs.symlinkJoin
-        {
-          name = "dev-environment";
-          paths = [
-            plugin
-            # plugin.buildInputs
-            pkgs.tmux
-            pkgs.bashInteractive
-            pkgs.busybox
-          ] ++ devPackages;
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        devPackages = [
+          (pkgs.bats.withLibraries (p: [ p.bats-support p.bats-assert ]))
+          pkgs.watchexec
+        ];
+        plugin = pkgs.tmuxPlugins.mkTmuxPlugin {
+          pluginName = "session-wizard";
+          rtpFilePath = "session-wizard.tmux";
+          version = "unstable";
+          src = self;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            substituteInPlace $target/session-wizard.tmux \
+              --replace  \$CURRENT_DIR/session-wizard.sh $target/session-wizard.sh
+            wrapProgram $target/session-wizard.sh \
+              --prefix PATH : ${with pkgs; lib.makeBinPath ([ fzf zoxide coreutils gnugrep gnused ])}
+          '';
         };
+      in
+      {
+        packages.dev = (pkgs.symlinkJoin
+          {
+            name = "dev-environment";
+            paths = [
+              plugin
+              plugin.buildInputs
+              pkgs.tmux
+              pkgs.bashInteractive
+              pkgs.busybox
+            ] ++ devPackages;
+          });
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = devPackages;
-      };
-    };
+        devShell = pkgs.mkShell {
+          buildInputs = devPackages;
+        };
+      }
+    );
 }
